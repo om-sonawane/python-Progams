65423d6f31c507ba0296f4430fc97ba3b2b3f509
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-11-25 02:21:01 -0800

    new

---
+++ else-if.py
@@ -0,0 +1,17 @@
+#var1=6
+#var2=56
+
+#var3=int(input())
+
+
+'''if var3>var2:
+    print("Greater")
+
+elif var3==var2:
+    print("Equal")
+else:
+    print("Lesser")'''
+
+listl=[5,7,3]
+if 5 in listl:
+    print("yes its in the list")
