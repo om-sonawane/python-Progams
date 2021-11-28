3f744db4f2c2604321de52ee54e87dac479ad7db
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-11-27 23:00:02 -0800

    faulty calculator

--- faulty-calc.py
+++ faulty-calc.py
@@ -1,2 +1,43 @@
 #design a claculator which will correctly solve all the problem except 
 #the following ones:-
+'''      
+45*3=555, 56+9=77,56/6=4
+
+your programm should take oprator and the two number as input from two number
+and return the result
+
+'''
+
+# its definitely  work without any Quary
+# making a faulty calculator
+
+# taking input from the user and changing into integer
+inp1 = input("Operator")
+
+
+# taking 2nd input from user
+inp2 = input("First number")
+
+# taking 3rd number
+inp3 = input("Last number")
+
+# making a new variable
+new_input = inp2 + inp1 + inp3
+
+# checking the input
+if new_input == "45*3":
+    print(555)
+elif new_input == "56+9":
+    print(77)
+elif new_input == "56/6":
+    print(4)
+elif inp1 == "*":
+    print(int(inp2) * int(inp3))
+elif inp1 == "+":
+    print(int(inp2) + int(inp3))
+elif inp1 == "-":
+    print(int(inp2) - int(inp3))
+elif inp1 == "/":
+    print(int(inp2) / int(inp3))
+
+
