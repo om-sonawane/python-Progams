9a028471c894aebc3239f6a50d887dde077c35d0
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-11-30 03:27:51 -0800

    new

--- loops.py
+++ loops.py
@@ -1,7 +1,12 @@
-for i in range(10):
+'''for i in range(10):
     print(i)
     if i == 5 :
         continue
-    print(i)       
+    print(i) '''
+
+listl =[["harry",1]["larry",2]["carry",6]["marie",250]]
+
+for item, in listl:
+    print(item)
 
   
