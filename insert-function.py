8ec68e8839cf66bc0102c7bcb7dd8256f3343dcb
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-12-05 21:55:42 -0800

    new

--- insert-function.py
+++ insert-function.py
@@ -1,8 +1,8 @@
 numbers=[2,7,9,11,3]
 #numbers.insert(2,56)
 #mutable
-#tp
-'''sort(): Sorts the list in ascending order.
+ #tp
+ '''sort(): Sorts the list in ascending order.
 type(list): It returns the class type of an object.
 append(): Adds a single element to a list.
 extend(): Adds multiple elements to a list.
@@ -12,5 +12,5 @@ min(list): It returns an item from the list with min value.
 len(list): It gives the total length of the list.
 list(seq): Converts a tuple into a list.
 cmp(list1, list2): It compares elements of both lists list1 and list2'''
-numbers.pop()
-print(numbers)
+ numbers.pop()
+ print(numbers)
